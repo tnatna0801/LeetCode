a98796bf16d1183d507adef22efb5d72e556f903
Soomin Kim <48270067+tnatna0801@users.noreply.github.com>
2022-06-28 22:16:38 +0900

    Time: 1 ms, Memory: 64.4 MB - LeetHub

--- rotate-array/rotate-array.java
+++ rotate-array/rotate-array.java
@@ -1,11 +1,9 @@
 // nums 배열 자체를 뒤집을 생각을 못해내서 오래걸렸다. 또다른 배열을 만들었더니 나중에 nums에 대입할때 결과로 Output Limit Exceeded 받았다. => reverse() 함수를 만듬.
-// index를 벗어났다고 runtime error가 떠버렸다. =>       
+// index를 벗어났다고 runtime error가 떠버렸다. => k가 nums의 크기보다 클 때 예외처리   
 class Solution {
     public void rotate(int[] nums, int k) {
         
-        System.out.println( k );
         k %= nums.length;
-        System.out.println( k );
         // 1. 배열 뒤집기
         reverse(nums, 0, nums.length-1);
         
