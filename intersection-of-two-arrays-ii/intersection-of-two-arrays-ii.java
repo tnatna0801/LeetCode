10db658ee57801398e29af24e67b9de4f4f6c764
Soomin Kim <48270067+tnatna0801@users.noreply.github.com>
2022-06-30 00:09:02 +0900

    Time: 10 ms, Memory: 44.7 MB - LeetHub

---
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+        if(nums1.length >= nums2.length){
+            return solution(nums1, nums2);
+        }
+        else {
+            return solution(nums2, nums1);
+        }
+    }
+    
+    public int[] solution(int[] nums1, int[] nums2){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> tmp = new ArrayList<>();
+        
+        for(int num:nums1){
+            if(map.containsKey(Integer.valueOf(num))){
+                 map.put(Integer.valueOf(num), map.get(Integer.valueOf(num))+1);
+            }
+            else{
+                map.put(Integer.valueOf(num), 1);
+            }
+        }
+        
+        for(int num:nums2){
+            if(map.containsKey(Integer.valueOf(num)) && map.get(Integer.valueOf(num)) > 0){
+                map.put(Integer.valueOf(num), map.get(Integer.valueOf(num))-1);
+                tmp.add(num);
+            }
+        }
+        
+        return tmp.stream().mapToInt(i -> i).toArray();
+    }
+}
