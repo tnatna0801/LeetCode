46a2df40b88d2f02886a3a1c9e85c1555243c854
Soomin Kim <48270067+tnatna0801@users.noreply.github.com>
2022-07-06 13:16:09 +0900

    Time: 8 ms, Memory: 47.6 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,30 +3,18 @@ import java.util.*;
 class Solution {
     /*
     output: 중복을 제거한 배열 길이, 중복을 제거한 배열
-    공간복잡도: O(1) 유지 ==> 새로운 배열을 만들지 X
+    공간복잡도: O(1) 유지
     배열은 오름차순으로 정렬되어있음
     */
     public int removeDuplicates(int[] nums) {
         int result = 0;
-        for(int i = 0; i<nums.length; i++){
-            int num = nums[i];
-            if(num == 101){
-                break;
+        for(int i = nums.length-1; i>0; i--){
+            if(nums[i] == nums[i-1]){
+                nums[i] = 101;
+                result++;
             }
-            for(int j = i; j<nums.length; j++){
-                if(num != nums[j]){
-                    i = j-1;
-                    break;
-                }
-                else if(num == nums[j]){
-                    nums[j] = 101;
-                    result--;
-                }
-            }
-            nums[i] = num;
-            result++;
         }
         Arrays.sort(nums);
-        return nums.length + result; 
+        return nums.length - result;
     }
 }
