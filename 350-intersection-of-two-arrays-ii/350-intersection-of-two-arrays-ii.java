f7772c3c2d2593dbcbec46df34d2973d455ef288
Soomin Kim <48270067+tnatna0801@users.noreply.github.com>
2022-07-07 19:43:14 +0900

    Time: 9 ms (14.72%), Space: 44.1 MB (43.20%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,17 +1,7 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         
-        // if(nums1.length >= nums2.length){
-        //     return solution(nums1, nums2);
-        // }
-        // else {
-        //     return solution(nums2, nums1);
-        // }
-        return solution(nums1, nums2);
-    }
-    
-    public int[] solution(int[] nums1, int[] nums2){
-        HashMap<Integer, Integer> map = new HashMap<>();
+         HashMap<Integer, Integer> map = new HashMap<>();
         ArrayList<Integer> tmp = new ArrayList<>();
         
         for(int num:nums1){
@@ -27,4 +17,5 @@ class Solution {
         
         return tmp.stream().mapToInt(i -> i).toArray();
     }
+    
 }
