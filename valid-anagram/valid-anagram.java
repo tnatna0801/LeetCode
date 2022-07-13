ff3e3905804ae065225b74a7f72c847422f15623
Soomin Kim <48270067+tnatna0801@users.noreply.github.com>
2022-07-13 18:15:02 +0900

    Time: 1 ms, Memory: 42.1 MB - LeetHub

--- valid-anagram/valid-anagram.java
+++ valid-anagram/valid-anagram.java
@@ -1,13 +1,17 @@
+//소문자만 => 26개
+//toCharArray(): 문자열을 char형 배열로 바꿔줌
+//배열에 각 알파벳의 횟수를 저장
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         int[] array = new int[26];
 
         for (char c : s.toCharArray()) {
-            array[c - 'a']++;
+            array[c - 97] += 1;
         }
 
         for (char c : t.toCharArray()) {
-            array[c - 'a']--;
+            array[c - 97] -= 1;
         }
 
         for (int e : array) {
